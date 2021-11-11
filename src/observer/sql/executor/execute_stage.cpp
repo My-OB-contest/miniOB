c2d7fbd5f5c09b1cbb2219911f16738ff6493c59
zhouhuahui <2957286012@qq.com>
2021-11-12 01:48:54 +0800

    binary test

--- src/observer/sql/executor/execute_stage.cpp
+++ src/observer/sql/executor/execute_stage.cpp
@@ -572,8 +572,8 @@ RC ExecuteStage::do_select(const char *db, Query *sql, SessionEvent *session_eve
   Trx *trx = session->current_trx();
   const AdvSelects &adv_selects = sql->sstr.adv_selection;
   Selects selects;
-  // if(!convert_to_selects(adv_selects, selects)) {
-  if(true){
+  if(!convert_to_selects(adv_selects, selects)) {
+  // if(true){
     // 走表达式路线;
     // TupleSet res_tupleset;
     // std::stringstream ss;
