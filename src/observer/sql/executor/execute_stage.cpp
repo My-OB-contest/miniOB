0337762bed162ffc2992d3bc0fb93290f37c002c
zhouhuahui <2957286012@qq.com>
2021-11-12 00:25:26 +0800

    just for binary test

--- src/observer/sql/executor/execute_stage.cpp
+++ src/observer/sql/executor/execute_stage.cpp
@@ -576,7 +576,7 @@ RC ExecuteStage::do_select(const char *db, Query *sql, SessionEvent *session_eve
     // 走表达式路线;
     TupleSet res_tupleset;
     std::stringstream ss;
-    rc = do_advselects(trx, adv_selects, db, res_tupleset);
+    // rc = do_advselects(trx, adv_selects, db, res_tupleset);
     if(rc != RC::SUCCESS) {
       end_trx_if_need(session, trx, true);
       return rc;
@@ -1139,7 +1139,7 @@ RC ExecuteStage::do_advselects(Trx *trx, const AdvSelects &adv_selects, const ch
   if(rc != RC::SUCCESS) {
     return rc;
   }
-  // rc = esnode->execute(res_tupleset);
+  rc = esnode->execute(res_tupleset);
   if(rc != RC::SUCCESS) {
     LOG_ERROR("ExpSelectExeNode::execute() runs wrong \n");
     delete esnode;
